506b8855f22d6c4e751bd430705d01df5b293e7e
soerenssch <soeren.schlisske@web.de>
2023-05-09 17:01:32 +0200

    update

--- Test.py
+++ Test.py
@@ -235,6 +235,8 @@ if input_method == SentimentAnalyse:
     if file is not None:
         df = pd.read_csv(file)
         df = df.dropna()
+
+        
         df["Datum"] = pd.to_datetime(df["Datum"])
 
         start_date = st.date_input("Start der Auswertung:", value=pd.to_datetime(df['Datum']).min().date())
@@ -276,7 +278,7 @@ if input_method == SentimentAnalyse:
             @st.cache_data(ttl=600)
             def generate_proscons_list(text):
                 word_blocks = text.split(' ')
-                block_size = 1750
+                block_size = 2000
                 blocks = [' '.join(word_blocks[i:i + block_size]) for i in range(0, len(word_blocks), block_size)]
 
                 proscons = []
